e9b325124a85a3915d30145822df9f66017776a2
yailin10 <yailincbtis16@gmail.com>
2018-05-28 07:39:22 -0500

    6subida

--- js/acciones3.js
+++ js/acciones3.js
@@ -1,10 +1,10 @@
-// JavaScript Document
+﻿// JavaScript Document
 $(document).ready(function(e) {
     document.addEventListener("deviceready",function(){ //escuchador de eventos
 		$('#izquierda').on("swipeleft",function(){ //deslizar a la izquiera
 			navigator.notification.alert("deslizó hacia la izquierda",function(){"aplicacion 7", "OK"}); //aparece mensaje alert
 		}); //cierraswipe inquierdo
-		$('#Derecha').on("swiperight",function(){ //deslizar la derecha
+		$('#derecha').on("swiperight",function(){ //deslizar la derecha
 			navigator.notification.confirm("¿Que quieres hacer?",function(op){ //aparece mensaje para confirmar
 				switch(op) //variable op para las acciones
 				{
